dcdeef015d4ce028b07e9d7bd1b68ceaf6bf7e74
16180600302 <baijinfeng1202@gmail.com>
2019-04-08 16:34:10 +0800

    final update directionary

---
+++ neusoftMall/neuMall-mall/src/main/java/com/neusoft/xiaobai/NeuMallApplication.java
@@ -0,0 +1,18 @@
+package com.neusoft.xiaobai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: xiaobai
+ * @Date: 2019/4/8 16:27
+ * @email: baijinfeng1202@gmail.com
+ * @address: 大连东软信息学院
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class NeuMallApplication  {
+    public static void main(String[] args) {
+        SpringApplication.run(NeuMallApplication.class,args);
+    }
+}
