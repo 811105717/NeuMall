1c544247568741714b52a68477c64a9d4d77a6e1
16180600302 <baijinfeng1202@gmail.com>
2019-04-08 17:50:18 +0800

    xiaobai created his code dir

---
+++ neusoftMall/neuMall-mall/src/main/java/com/neusoft/mall/orderCenter/controller/OrderController.java
@@ -0,0 +1,18 @@
+package com.neusoft.mall.orderCenter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: xiaobai
+ * @Date: 2019/4/8 17:49
+ * @email: baijinfeng1202@gmail.com
+ * @address: 大连东软信息学院
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping(value = "/front/orderCenter")
+public class OrderController {
+
+
+}
