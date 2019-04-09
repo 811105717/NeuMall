0cd39f397bd633c46b8feba30317f2ec2baf1e4f
16180600302 <baijinfeng1202@gmail.com>
2019-04-09 13:17:09 +0800

    xiaobai created his code dir and classes(interface)

---
+++ neusoftMall/neuMall-mall/src/main/java/com/neusoft/mall/userSettings/controller/UserSettingController.java
@@ -0,0 +1,22 @@
+package com.neusoft.mall.userSettings.controller;
+
+import com.neusoft.mall.userSettings.service.UserSettingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: xiaobai
+ * @Date: 2019/4/9 13:12
+ * @email: baijinfeng1202@gmail.com
+ * @address: 大连东软信息学院
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/front/passWord")
+public class UserSettingController {
+
+    @Autowired
+    private UserSettingService userSettingService;
+
+}
