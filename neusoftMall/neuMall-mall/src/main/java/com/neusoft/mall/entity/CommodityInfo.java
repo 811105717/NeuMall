b75e6ff5d14462333cb66adbe623ea81e71ec099
16180600302 <baijinfeng1202@gmail.com>
2019-04-26 13:18:10 +0800

    修复张强的登陆bug  --xiaobai

--- neusoftMall/neuMall-mall/src/main/java/com/neusoft/mall/entity/CommodityInfo.java
+++ neusoftMall/neuMall-mall/src/main/java/com/neusoft/mall/entity/CommodityInfo.java
@@ -27,6 +27,7 @@ public class CommodityInfo extends BaseEntity {
     private String commodityTotalCount; //销量
     private String categoryFirst; //一级分类ID
     private String categorySecond; //二级分类ID
+    private String pictureAddress;
 
     public String getCommodityCode() {
         return commodityCode;
