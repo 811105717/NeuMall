03083f5218e35b76bbac3a160a0cf19057721e1f
16180600506 <16180600506>
2019-05-06 13:29:37 +0800

    5019/5/6  13:29

--- neusoftMall/neuMall-mall/src/main/java/com/neusoft/mall/goods/entity/param/DeleteGoods.java
+++
@@ -1,9 +0,0 @@
-package com.neusoft.mall.goods.entity.param;
-
-import lombok.Data;
-
-@Data
-public class DeleteGoods {
-    private String commodityId;
-    private String tokenBackend;
-}
--- neusoftMall/neuMall-mall/src/main/java/com/neusoft/mall/goods/entity/param/GetGoodsDetail.java
+++
@@ -1,9 +0,0 @@
-package com.neusoft.mall.goods.entity.param;
-
-import lombok.Data;
-
-@Data
-public class GetGoodsDetail {
-    private String commodityId;
-    private String tokenBackend;
-}
